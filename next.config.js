cdc53731e1d13e180a61de18da8ca45c5f20157f
whatleyconsultants-eng <whatleyconsultants@gmail.com>
2026-03-15 11:40:08 -0400

    Revert "Add portal files"
    This reverts commit dd3c338dee3681ff7afdcdf820f64aab6c66dd68.

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['your-storage-domain.com'], // Add your S3/storage domain here
-  },
-}
-
-module.exports = nextConfig
